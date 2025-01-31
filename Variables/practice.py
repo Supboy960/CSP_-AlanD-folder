7548d71dcd379b7a06b4d6107bdfa0b4b691c631
Supboy960 <Alan.delar@ucas-edu.net>
2025-01-31 11:14:54 -0700

    save

--- Variables/practice.py
+++ Variables/practice.py
@@ -18,6 +18,8 @@ this_year = 2025
 
 age = 14
 
+eye_color = "brown"
+
 fav_subject= "Biology"
 
-print(name,"was born",year_born,"Has had",breakfast,"for breakfast and like's the color",favorite_color,"Goes to", school_name, "is",age, "years old in", this_year, fav_subject, "is the best he is in at UCAS", this_year, "hope's to have,", money, " doller before 18 years old",)
+print(name,"was born",year_born,"Has had",breakfast,"for breakfast and like's the color",favorite_color, "my eye color is", eye_color, "Goes to", school_name, "is",age, "years old in", this_year, fav_subject, "is the best he is in at UCAS", this_year, "hope's to have,", money, " doller before 18 years old",)
--- template.c
+++ template.c
@@ -4,6 +4,7 @@ char name[] = "Alan";
 int age = 14;
 float pi = 3.14;
 
+
 int main(void){
     printf("Hello i am %s. I am %d years old. I like the %f.\n", name, age, pi);
     printf("%d\n", age);
