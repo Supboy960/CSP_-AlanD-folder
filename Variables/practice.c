e0c2f5a9e974e55bf472928d589284129b77c0a7
Supboy960 <Alan.delar@ucas-edu.net>
2025-01-31 11:41:27 -0700

    save

---
+++ Variables/practice.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+char name[]= "Alan";
+
+int number= 18;
+
+int money=1000;
+
+char breakfast[]= "nothing";
+
+char favorite_color[]= "green";
+
+char school_name[]= "UCAS";
+
+char year_born[]= "2010";
+
+int this_year = 2025;
+
+int age = 14;
+
+char eye_color[] = "brown";
+
+char fav_subject[]= "Biology";
+
+int main(void){
+    printf("%s was born %d, has had %s, for breakfast and like's the color %s, My eye color is %s, Goes to %s, is %d, years old in %d, %s is the best he is in at UCAS (for now), %d hope's to have, %d doller before 18 years old", name,year_born,breakfast,favorite_color, eye_color, school_name,age, this_year, fav_subject, this_year, money);
+
+    return 0;
+}
