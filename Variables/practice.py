71b9d1e3a05a7c79301fdf2092a18b21a7649bc2
Supboy960 <Alan.delar@ucas-edu.net>
2025-01-31 11:05:46 -0700

    Save

--- Variables/practice.py
+++ Variables/practice.py
@@ -14,4 +14,10 @@ school_name= "UCAS"
 
 year_born= "2010"
 
-print(name,"was born",year_born,"Has had",breakfast,"for breakfast and like the color",favorite_color,"Goes to", school_name)
+this_year = 2025
+
+age = 14
+
+fav_subject= "Biology"
+
+print(name,"was born",year_born,"Has had",breakfast,"for breakfast and like's the color",favorite_color,"Goes to", school_name, "is",age, "years old in", this_year, fav_subject, "is the best he is in at UCAS", this_year )
