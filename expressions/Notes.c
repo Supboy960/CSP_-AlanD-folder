45488e5d92d75151f2270e1212f8f7bc3c78822c
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-03 11:49:21 -0700

    save

---
+++ expressions/Notes.c
@@ -0,0 +1,11 @@
+// Alan De Lara, Expression Notes C
+#include <stdio.h>
+#include <math.h>
+float eqation = 5*7+11/(5-x):
+Flaot eqa
+// + - * / 
+int main(void){
+
+    //printf("%f", eauation);
+    return 0;
+}
