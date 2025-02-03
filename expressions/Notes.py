d33fbde3b220758550815babc15e6ba27c254044
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-03 11:43:10 -0700

    save

--- expressions/Notes.py
+++ expressions/Notes.py
@@ -1,2 +1,20 @@
 #Alan De Lara, Expressions Notes Python
 
+print(70/8)
+print(70+8)
+print(70-8)
+print(70*8)
+print(70**8)
+print(70//8) #interger divion returns just the whole number
+print(70&8) #modulo/Moduler/mod retun the resunter
+
+num = 4
+
+print(num)
+
+num+= 2
+
+print(num)
+
+
+
