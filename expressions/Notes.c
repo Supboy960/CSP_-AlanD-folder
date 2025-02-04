fb2390c5102245d69f853e8560a1b6772fbac3be
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-04 11:16:55 -0700

    SAVE 2/4/2025

--- expressions/Notes.c
+++ expressions/Notes.c
@@ -1,11 +1,15 @@
 // Alan De Lara, Expression Notes C
 #include <stdio.h>
 #include <math.h>
-float eqation = 5*7+11/(5-x):
-Flaot eqa
+//float x = pow(2,2);
+//float eqation = 5*7+11/(5-x):
+int num = 12;
+float pi = 3.1415926535;
 // + - * / 
 int main(void){
-
+//num = 4;
+//printf("%d\n", num);
+printf("%.20f", pi);
     //printf("%f", eauation);
     return 0;
 }
--- expressions/Notes.py
+++ expressions/Notes.py
@@ -1,20 +1,24 @@
 #Alan De Lara, Expressions Notes Python
 
-print(70/8)
-print(70+8)
-print(70-8)
-print(70*8)
-print(70**8)
-print(70//8) #interger divion returns just the whole number
-print(70&8) #modulo/Moduler/mod retun the resunter
+#print(70/8)
+#print(70+8)
+#print(70-8)
+#print(70*8)
+#print(70**8)
+#print(70//8) #interger divion returns just the whole number
+#print(70&8) #modulo/Moduler/mod retun the resunter
 
 num = 4
 
-print(num)
+#print(num)
 
 num+= 2
 
-print(num)
+#print(num)
+
+pi = 3.1415926535
+
+print (f"{pi:.4f}")
 
 
 
