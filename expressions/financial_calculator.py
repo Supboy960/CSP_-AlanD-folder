f7aaafb2fe8b3ee1025ea08b2eb4f8b8be0397ed
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-06 11:49:16 -0700

    2/6/2025

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -7,23 +7,23 @@ income = float (input ("what is your income?\n"))
 #ask what their rent is (varible an inputs)
 rent = float (input ("what is your rent?\n"))
 #ask what their utlitities is (varible an inputs)
-utlitities = float (input ("what is your utlitities?\n"))
+utilities = float (input ("what is your utlitities?\n"))
 #ask what their groceries is (varible an inputs)
 groceries = float (input ("what is your groceries?\n"))
 #ask what their transportation is (varible an inputs)
 transportation = float (input ("what is your transportation?\n"))
 #calulate savings as 10% of income(income*.1) (variable)
-
+savings = (income/10)
 #Calualte spening as income-saving-rent-utilites-grocries-transpotation (variable)
-
+spending = (income-(rent-utilities-groceries-transportation))
 #calulate percent income of rent (rent/income *100) (varible)\
-
+income_of_rent = (rent/income *100)
 #calulate percent income of untilaiyes (utilalies/income *100) (varible)
-
+income_of_utilities = (utilities/income *100)
 #calulate percent income of gorceries (gorceries/income *100) (varible)
-
+income_of_groceries = (groceries/income *100)
 ##calulate percent income of transportation (tansportation/income *100) (varible)
-
+income_of_transportation = (transportation/income *100)
 #calulate percent income of spending (spending/income *100) (varible)
 
 #Your rent is $XX.XX which is XX% of income. (print)
