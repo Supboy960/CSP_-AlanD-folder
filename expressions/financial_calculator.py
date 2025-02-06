5ccaa7d5a732905d3243d0026f0ace713242da51
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-06 11:40:26 -0700

    save

---
+++ expressions/financial_calculator.py
@@ -0,0 +1,39 @@
+# Alan De Lara, Financial Calculator python 
+
+# print statment the welcomes user and tells what progame does
+
+#ask what their income is (varible an inputs)
+income = float (input ("what is your income?\n"))
+#ask what their rent is (varible an inputs)
+rent = float (input ("what is your rent?\n"))
+#ask what their utlitities is (varible an inputs)
+utlitities = float (input ("what is your utlitities?\n"))
+#ask what their groceries is (varible an inputs)
+groceries = float (input ("what is your groceries?\n"))
+#ask what their transportation is (varible an inputs)
+transportation = float (input ("what is your transportation?\n"))
+#calulate savings as 10% of income(income*.1) (variable)
+
+#Calualte spening as income-saving-rent-utilites-grocries-transpotation (variable)
+
+#calulate percent income of rent (rent/income *100) (varible)\
+
+#calulate percent income of untilaiyes (utilalies/income *100) (varible)
+
+#calulate percent income of gorceries (gorceries/income *100) (varible)
+
+##calulate percent income of transportation (tansportation/income *100) (varible)
+
+#calulate percent income of spending (spending/income *100) (varible)
+
+#Your rent is $XX.XX which is XX% of income. (print)
+
+#Your untilyes is $XX.XX which is XX% of income. (print)
+
+#Your grociers is $XX.XX which is XX% of income. (print)
+
+#Your transportation is $XX.XX which is XX% of income. (print)
+
+#Your savings is $XX.XX which is XX% of income. (print)
+
+#Your spending is $XX.XX which is XX% of income. (print)
