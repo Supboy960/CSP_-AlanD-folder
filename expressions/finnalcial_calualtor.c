9a4f40b7233177321bfed684e450b442775d839b
Supboy960 <Alan.delara@ucas-edu.net>
2025-02-08 02:47:58 +0000

    2/7/2025

--- expressions/finnalcial_calualtor.c
+++ expressions/finnalcial_calualtor.c
@@ -2,13 +2,14 @@
 
 #include <stdio.h>
 
+
 int main(void) {
     // Print statement that welcomes the user and tells what the program does
     printf("Welcome to the Financial Calculator! This program will help you analyze your income and expenses.\n");
 
     // Declare variables for income and expenses
     float income, rent, utilities, groceries, transportation, savings, spending;
-    float percent_of_rent, percent_of_utilities, percent_of_groceries, percent_of_transportation, percent_of_spending, savings_percentage;
+    float percent_of_rent, percent_of_utilities, percent_of_groceries, percent_of_transportation, percent_of_savings, percent_of_spending;
 
     // Ask for the user's income and expenses
     printf("What is your income?\n");
@@ -37,7 +38,7 @@ int main(void) {
     percent_of_utilities = (utilities / income) * 100;
     percent_of_groceries = (groceries / income) * 100;
     percent_of_transportation = (transportation / income) * 100;
-    savings_percentage = (savings / income) * 100;
+    percent_of_savings = (savings / income) * 100;
     percent_of_spending = (spending / income) * 100;
 
     // Print the results
@@ -45,7 +46,7 @@ int main(void) {
     printf("Your utilities are $%.2f which is %.2f%% of your income.\n", utilities, percent_of_utilities);
     printf("Your groceries are $%.2f which is %.2f%% of your income.\n", groceries, percent_of_groceries);
     printf("Your transportation is $%.2f which is %.2f%% of your income.\n", transportation, percent_of_transportation);
-    printf("Your savings is $%.2f which is %.2f%% of your income.\n", savings, savings_percentage);
+    printf("Your savings is $%.2f which is %.2f%% of your income.\n", savings, percent_of_savings);
     printf("Your spending is $%.2f which is %.2f%% of your income.\n", spending, percent_of_spending);
 
     return 0;
