15b79f0afffa9b024ff63fbcdfd1672d776da5dd
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-10 11:49:01 -0700

    save

---
+++ string/notes.py
@@ -0,0 +1,14 @@
+# Alan De Lara, Strings Motes Python
+
+#string is a data type that hold any info surrounded by qoutation marks "" ''
+
+note= "Alan's class" #yes string
+
+#name = input("What is your first name?").strip().capitalize()
+
+#print(f"Hello {name} well come to my program\n")
+
+print(note)
+
+sentace = "the quick brown fox jumps over the lazy dog"
+
