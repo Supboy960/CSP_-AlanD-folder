7924a432ede6d64f2a66311dbcd4b2a3d61da3dc
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-12 11:48:55 -0700

    Save

---
+++ string/name decorator.py
@@ -0,0 +1,10 @@
+#Alan De Lara, Name Decorator.C
+
+ 
+    
+   
+    name =  input ("what is you name:")
+    dec = "(:(:"
+    print("Welcome to the Name Decorator! This program will add text decorations on both sides of your name.\n")   
+    print(dec+name)  
+   
