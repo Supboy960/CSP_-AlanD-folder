7534ce4e82ee5f6c2dde53790588efe56554a1fc
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-12 11:42:29 -0700

    save

--- string/name_decorator.c
+++ string/name_decorator.c
@@ -1,22 +1,22 @@
 //Alan De Lara, Name Decorator.C
 
-#include <stdio.h>
-#include <string.h>
-
-char subject[50];
-char name[]= "";
-
-
-
-int main(void){
-printf("What is your name?\n");
-char one[]= "(: (:";
-char two[]= "World!";
-char three[]= ":) :)";
-printf("%s\n", one);
-strcat(one,two);
-printf("%s", one);
-strcat(two, three);//can only be cat two thing at a time
-printf("%s",three);
-    return 0;
-}
+#include <stdio.h>    
+#include <string.h>    
+    
+int main(void) {    
+    char name[50];   
+    
+    printf("Welcome to the Name Decorator! This program will add text decorations on both sides of your name.\n");    
+    printf("What is your name: ");    
+    fgets(name, sizeof(name), stdin);    
+    name[strcspn(name, "\n")] = 0;     
+    
+    printf("Hi %s\n", name);    
+    
+    char one[100] = "(: (: ";   
+    char two[] = " :) :)";    
+    strcat(one,name);
+    strcat(one,two);
+   printf(one);
+    return 0;    
+}  
