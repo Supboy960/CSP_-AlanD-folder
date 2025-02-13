0237c6c2156b0ea3e150af6e6dde293984ad6f2d
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-13 11:18:13 -0700

    save

---
+++ string/silly_sentences.c
@@ -0,0 +1,17 @@
+//Alan De Lara, Silly Sentences - C
+#include <stdio.h>
+//empty string varibaes for users (Minum 3)
+
+
+int main(void){
+    // A welcome for the user telling them what the progtagam is (pirint)
+char name[50];
+printf("Hi %s\n", name);
+    //ASK USER FOR WORDS (PRINT STATEMENT WITHA QESTION SACN F TO SET TO VARIBALE) (ine c we need to tell user that they can oly tpye 1 word)
+
+    //pint out the stor whit the varibales inserted (" weclome %s to may prgeam",  name) One print atament
+
+
+    printf("It was a" "morings when the" "come to pick me up for" " The");
+    return 0;
+}
