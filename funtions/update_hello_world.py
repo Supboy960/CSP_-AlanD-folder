03ce01407bf5a9ad8f5f3290c0a61b5b30afdec8
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-19 11:49:20 -0700

    SAVE

---
+++ funtions/update_hello_world.py
@@ -0,0 +1,11 @@
+# Alan De Lara, Update Hello World.Python
+
+
+
+subject = input("What's your favorite suject: ")
+
+small_number = 4
+
+name=  input ("what is you name:")
+
+print(name, "really likes", subject)
