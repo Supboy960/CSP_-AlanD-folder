0851ce0ff4942f892f3d9d3eb28474cc2affeaa2
Supboy960 <Alan.delara@ucas-edu.net>
2025-02-20 05:16:02 +0000

    save

---
+++ funtions/update_financial_calculator.py
@@ -0,0 +1,14 @@
+#Alan De Lara, Update Financial Calculator - Python
+print("Hello, this is my finantial calculator that calculates savings, spendins, and other finantial expenses.")
+
+def collecting_data(type):
+    return input(f"What is your monthly {type} cost:\n")
+    
+    
+income = float(input("What is your monthly income:\n"))
+rent = collecting_data("rent")
+utilities = collecting_data("utilities")
+groceries = collecting_data("groceries")
+transportation = collecting_data("transportation")
+
+savings = income*0.1
