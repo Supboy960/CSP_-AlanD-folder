41015189951e35b8df6be8c7741aa1f1f6bd0993
Supboy960 <Alan.delara@ucas-edu.net>
2025-02-22 01:27:10 +0000

    save

--- funtions/update_your_financial_calculator.C
+++ funtions/update_your_financial_calculator.C
@@ -41,14 +41,8 @@ float percent_of_utilities = display(utilities, income, "utilities");
 float percent_of_groceries = display(groceries, income, "groceries");
 float percent_of_transportation = display(transportation, income, "transportation");
 float percent_of_savings = display(savings, income, "savings");
-float  percent_of_spending = display(spending, income, "spending");
+float percent_of_spending = display(spending, income, "spending");
  
-printf("Your rent is $%.2f which is %.2f%% of your income.\n", rent, per);  
-printf("Your utilities are $%.2f which is %.2f%% of your income.\n", utilities, percent_of_utilities);  
-printf("Your groceries are $%.2f which is %.2f%% of your income.\n", groceries, percent_of_groceries);  
-printf("Your transportation is $%.2f which is %.2f%% of your income.\n", transportation, percent_of_transportation);  
-printf("Your savings is $%.2f which is %.2f%% of your income.\n", savings, percent_of_savings);
-printf("Your spending is $%.2f which is %.2f%% of your income.\n", spending, percent_of_spending);
 
     
     
