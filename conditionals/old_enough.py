7a6a229606261519c1913ecee50df545c93dd58b
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-26 11:39:22 -0700

    save

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -5,7 +5,7 @@ print("Welcome to my old enough program that tells you whether you are old enoug
 age = input("What is your age(in years!):\n")
 
 if age < 5:
-    print("You aren't old enough to go to school, 6you can't get your get your learner's permit, drive, or vote.\nHow are you running this program?\nHow can you even read this?!?\n")
+    print("You aren't old enough to go to school, you can't get your get your learner's permit, drive, or vote.\nHow are you running this program?\nHow can you even read this?!?\n")
 elif age >= 18:
     print("You are old enough to vote, drive, get your learner's permit, and (should be) go to school.\n")
 elif age >= 16:
