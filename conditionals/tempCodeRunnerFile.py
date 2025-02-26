3f3f92a07f0707a60535f9d7dd87029f9d4712a8
Supboy960 <Alan.delar@ucas-edu.net>
2025-02-26 11:42:29 -0700

    save

--- conditionals/tempCodeRunnerFile.py
+++ conditionals/tempCodeRunnerFile.py
@@ -2,7 +2,7 @@
 
 print("Welcome to my old enough program that tells you whether you are old enough to vote, drive, get a learners permit, and go to school. (Don't take the advice given seriously from this program, this only for entertainment purposes only.)")
 
-age = int(input("What is your age(in years!):\n"))
+age = input("What is your age(in years!):\n")
 
 if age < 5:
     print("You aren't old enough to go to school, you can't get your get your learner's permit, drive, or vote.\nHow are you running this program?\nHow can you even read this?!?\n")
