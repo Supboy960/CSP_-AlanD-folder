0e1f188f67a178da7e6a67865596f1f248895a4e
Supboy960 <Alan.delara@ucas-edu.net>
2025-02-27 19:25:46 +0000

    save

--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,28 +1,13 @@
 #Alan De Lara, Time of Day - Python
-import time    
-    
-    local_time = time.localtime(current)
-day = local_time.tm_wday
-hour = local_time.tm_hour #millitary time (0-23)
-print(hour)
 
-print("Hello! This program tells you the time of day.")    
-hour = int(input("What time is it? (Enter in 24-hour format):\n "))    
-    
-if hour >= 0 and hour <= 11:    
-    print("Good morning!")    
-elif hour >= 12 and hour <= 17:    
-    print("Good afternoon!")    
-else:    
+import time
+current = time.time()
+local_time = time.localtime(current)
+hour = local_time.tm_hour # millitary time (0-23)
+
+if(hour <= 12):
+    print("Good morning!")
+elif(hour <= 17):
+    print("Good afternoon!")
+else:
     print("Good evening!")
-
-
-    print("Hello! This program tells you the time of day.")  
-hour = int(input("What time is it? (Enter in 24-hour format):\n "))    
-  
-if hour >= 0 and hour <= 11:    
-    print("Good morning!")    
-elif hour >= 12 and hour <= 17:    
-    print("Good afternoon!")    
-else:    
-    print("Good evening!")    
