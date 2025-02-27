9c5c0c01ecf2207954048ae599e1b868d0a2b779
Supboy960 <Alan.delara@ucas-edu.net>
2025-02-27 19:43:42 +0000

    save

--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,12 +1,10 @@
 #Alan De Lara, Time of Day - Python
 
 import time
-first_time = time.gmtime()
 current = time.time()
-now = time.ctime(current)
 local_time = time.localtime(current)
 hour = local_time.tm_hour # millitary time (0-23)
-print(hour)
+
 
 if(hour <= 12):
     print("Good morning!")
