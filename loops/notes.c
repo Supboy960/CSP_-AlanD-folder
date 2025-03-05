05020a166ac6b81c15e2e02c970f54b5b4fafd95
Supboy960 <Alan.delar@ucas-edu.net>
2025-03-05 11:11:58 -0700

    save

---
+++ loops/notes.c
@@ -0,0 +1,28 @@
+// Alan De Lara, loops notes C
+#include <stdio.h>
+
+
+
+int main(void){
+//1.What is a loop? 
+    //A section of code repested multiple times
+//2.What are the two types of loops?
+    //while loops
+    int start = 0;
+    while(start <5){
+        printf("Hello!\n");
+        start++;
+    }
+    //For loops
+    int q;
+    for(q=0;q<5;q++){
+        printf("%d\n",q);
+    }
+//3.What is iteration
+//4.What are lists? 
+//8.How do you make lists in C?
+//9.How do you make for loops in C?
+//10.How do you make while loops in C?
+    
+    return 0;
+}
