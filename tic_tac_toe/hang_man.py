6278eba3c54e0c3ef3f0524cfdafed73b9d74980
Supboy960 <Alan.delara@ucas-edu.net>
2025-03-27 17:49:20 +0000

    save

--- tic_tac_toe/hang_man.py
+++ tic_tac_toe/hang_man.py
@@ -2,8 +2,8 @@
 import random  
 
 # word list  
-words = ["pattern", "hay", "overview", "anniversary", "flourish", "remember", "experiment","arrange", "stadium", "domestic", "demonstration", "conversation", ]  
-  
+words = ["Blood", "dark", "scared", "scream", "drenched", "helpless", "experiment","choking", "crazed", "knife", "chainsaw", "gore", "corpse", "decomposed" ]  
+stages = 0
 def display_hangman(wrong_attempts):  
     if wrong_attempts == 0:  
         return """  
@@ -76,38 +76,44 @@ def check_guess(letter, chosen_word, guessed_letters):
     else:  
         return False  
   
-# Main game loop  
-while True:  
-    guessed_letters = []  # Reset guessed letters  
-    wrong_attempts = 0    # Reset wrong attempts  
-    max_attempts = 6      # max attempts  
-    chosen_word = random.choice(words)  # Choose a new word  
+# Main game loop    
+while True:    
+    guessed_letters = []  # Reset guessed letters    
+    wrong_attempts = 0    # Reset wrong attempts    
+    max_attempts = 6      # max attempts    
+    chosen_word = random.choice(words)  # Choose a new word    
+    
+    while wrong_attempts < max_attempts:  # Inner loop for guessing    
+        print(display_hangman(wrong_attempts))  # Display hangman    
+        display = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_word)    
+        print(display)  # Show the current state of the word    
+    
+        letter = input("Guess a letter: ")    
+    
+        if len(letter) == 1 and letter.isalpha():  # Validate input    
+            if letter in guessed_letters:    
+                print("You've already guessed that letter.")    
+            else:    
+                guessed_letters.append(letter)  # Add the letter if it's new  
+                print("Guessed letters:", guessed_letters)  # Show guessed letters  
   
-    while wrong_attempts < max_attempts:  # Inner loop for guessing  
-        print(display_hangman(wrong_attempts))  # Display hangman  
-        display = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_word)  
-        print(display)  # Show the current state of the word  
-  
-        letter = input("Guess a letter: ")  
-  
-        if len(letter) == 1 and letter.isalpha():  # Validate input  
-            if letter in guessed_letters:  
-                print("You've already guessed that letter.")  
-            else:  
-                if check_guess(letter, chosen_word, guessed_letters):  
-                    print("Good guess!")  
-                else:  
-                    wrong_attempts += 1  
-                    print("Wrong guess!")  
-  
-        if set(chosen_word).issubset(set(guessed_letters)):  # Win condition  
-            print("Congratulations! You've guessed the word:", chosen_word)  
-            break  
-  
-    if wrong_attempts == max_attempts:  # Loss condition  
-        print("Game over! The word was:", chosen_word)  
-  
-    # ask if they want to play again  
-    play_again = input("Do you want to play again? (yes/no) ")  
-    if play_again.lower() != "yes":  
-        break  
+                if check_guess(letter, chosen_word, guessed_letters):    
+                    print("Good guess!")    
+                else:    
+                    wrong_attempts += 1    
+                    print("Wrong guess!")    
+    
+        if set(chosen_word).issubset(set(guessed_letters)):  # Win condition    
+            print("Congratulations! You've guessed the word:", chosen_word)    
+            break    
+    
+    if wrong_attempts == max_attempts:  # Loss condition    
+        print("Game over! The word was:", chosen_word)    
+    
+    # ask if they want to play again    
+    play_again = input("Do you want to play again? (yes/no) ")    
+    if play_again.lower() != "yes":    
+        break    
+
+    (if wrong_attempts == 5:  
+    print("You have one more guess!")  )
--- tic_tac_toe/tempCodeRunnerFile.py
+++ tic_tac_toe/tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
 #Alan De Lara
 import random  
-  
-# Your original word list  
-words = ["pattern", "hay", "overview", "anniversary", "flourish", "remember", "experiment","arrange", "stadium", "domestic", "demonstration", "conversation", ]  
+
+# word list  
+words = ["Blood", "dark", "scared", "scream", "drenched", "helpless", "experiment","choking", "crazed", "knife", "chainsaw", "gore", "corpse", "decomposed" ]  
   
 def display_hangman(wrong_attempts):  
     if wrong_attempts == 0:  
@@ -40,7 +40,7 @@ def display_hangman(wrong_attempts):
           ------  
           |      |  
           |      O  
-          |     /|  
+          |     /|
           |        
           |        
         __|_________
@@ -50,31 +50,22 @@ def display_hangman(wrong_attempts):
           ------  
           |      |  
           |      O  
-          |     /|  
+          |     /|\\
           |      |  
           |        
-       ___|_________ 
+        __|_________ 
         """  
     elif wrong_attempts == 5:  
         return """  
           ------  
           |      |  
           |      O  
-          |     /|  
+          |     /|\\
           |      |  
-          |     /  
+          |     / \\
         __|_________ 
         """  
-    elif wrong_attempts == 6:  
-        return """  
-          ------  
-          |      |  
-          |      O  
-          |     /|\\  
-          |      |  
-          |     / \\  
-        __|_________  
-        """  
+    
 
     return stages[wrong_attempts]  
   
@@ -85,38 +76,41 @@ def check_guess(letter, chosen_word, guessed_letters):
     else:  
         return False  
   
-# Main game loop  
-while True:  
-    guessed_letters = []  # Reset guessed letters  
-    wrong_attempts = 0    # Reset wrong attempts  
-    max_attempts = 6      # Define max attempts  
-    chosen_word = random.choice(words)  # Choose a new word  
+# Main game loop    
+while True:    
+    guessed_letters = []  # Reset guessed letters    
+    wrong_attempts = 0    # Reset wrong attempts    
+    max_attempts = 6      # max attempts    
+    chosen_word = random.choice(words)  # Choose a new word    
+    
+    while wrong_attempts < max_attempts:  # Inner loop for guessing    
+        print(display_hangman(wrong_attempts))  # Display hangman    
+        display = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_word)    
+        print(display)  # Show the current state of the word    
+    
+        letter = input("Guess a letter: ")    
+    
+        if len(letter) == 1 and letter.isalpha():  # Validate input    
+            if letter in guessed_letters:    
+                print("You've already guessed that letter.")    
+            else:    
+                guessed_letters.append(letter)  # Add the letter if it's new  
+                print("Guessed letters:", guessed_letters)  # Show guessed letters  
   
-    while wrong_attempts < max_attempts:  # Inner loop for guessing  
-        print(display_hangman(wrong_attempts))  # Display hangman  
-        display = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_word)  
-        print(display)  # Show the current state of the word  
-  
-        letter = input("Guess a letter: ")  
-  
-        if len(letter) == 1 and letter.isalpha():  # Validate input  
-            if letter in guessed_letters:  
-                print("You've already guessed that letter.")  
-            else:  
-                if check_guess(letter, chosen_word, guessed_letters):  
-                    print("Good guess!")  
-                else:  
-                    wrong_attempts += 1  
-                    print("Wrong guess!")  
-  
-        if set(chosen_word).issubset(set(guessed_letters)):  # Win condition  
-            print("Congratulations! You've guessed the word:", chosen_word)  
-            break  
-  
-    if wrong_attempts == max_attempts:  # Loss condition  
-        print("Game over! The word was:", chosen_word)  
-  
-    # Optionally, ask if they want to play again  
-    play_again = input("Do you want to play again? (yes/no) ")  
-    if play_again.lower() != "yes":  
-        break  
+                if check_guess(letter, chosen_word, guessed_letters):    
+                    print("Good guess!")    
+                else:    
+                    wrong_attempts += 1    
+                    print("Wrong guess!")    
+    
+        if set(chosen_word).issubset(set(guessed_letters)):  # Win condition    
+            print("Congratulations! You've guessed the word:", chosen_word)    
+            break    
+    
+    if wrong_attempts == max_attempts:  # Loss condition    
+        print("Game over! The word was:", chosen_word)    
+    
+    # ask if they want to play again    
+    play_again = input("Do you want to play again? (yes/no) ")    
+    if play_again.lower() != "yes":    
+        break    
