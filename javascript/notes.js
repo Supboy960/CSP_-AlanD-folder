9d1266234e9747fd7006d99df3670bcb935accb6
Supboy960 <alan.delara@ucas-edu.net>
2025-04-22 11:46:16 -0600

    save

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,9 +1,9 @@
 
 
-let images = ["https://images.squarespace-cdn.com/content/v1/63c60d44c98af85334537583/644359f9-c213-4d77-bdb0-b78b7f8424f3/Mount_Timpanogos_at_sunset.jpg","https://i.etsystatic.com/37394532/r/il/27e6c7/5018857984/il_fullxfull.5018857984_l9h3.jpg" , "https://www.mountain-forecast.com/system/images/24500/large/Mount-Timpanogos.jpg?1532401039" 
+let images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcUZpl8OWqEIF8Ovv2ehl6d3IdaVWrKOm4w_VeeOK-4JN7RpHyQ&s=10&ec=72940545","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmI64E8Soa1kYDYzCrsOeQnoW2-w3MoHOT7g7otm9UqJAXKsFIbA&s=10&ec=72940545" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsisMjDMgcDz1sH_fQYqH6tq-7cT5vjgwtqjJUMSzC3kGBbPSTOg&s=10&ec=72940545" 
 ]
 function hello(){
-    document.getElementById("title").innerHTML = "Hello World!"
+    document.getElementById("title").innerHTML = "SUPRA! (Chargers suck!)"
 }
 count = 0
 function change(){
