d2732371fa5216a95ae03b3715af30de3f3b06e1
Supboy960 <alan.delara@ucas-edu.net>
2025-04-23 11:11:32 -0600

    save

--- javascript/notes.js
+++ javascript/notes.js
@@ -3,8 +3,9 @@
 let images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcUZpl8OWqEIF8Ovv2ehl6d3IdaVWrKOm4w_VeeOK-4JN7RpHyQ&s=10&ec=72940545","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmI64E8Soa1kYDYzCrsOeQnoW2-w3MoHOT7g7otm9UqJAXKsFIbA&s=10&ec=72940545" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsisMjDMgcDz1sH_fQYqH6tq-7cT5vjgwtqjJUMSzC3kGBbPSTOg&s=10&ec=72940545" 
 ]
 function hello(){
-    document.getElementById("title").innerHTML = "SUPRA! (Chargers suck!)"
-}
+     let name = window.prompt("What is your name?", "Koro Sensei")
+    document.getElementById("title").innerHTML = "Hello " + name + "! You know that Challegers suck"
+   
 count = 0
 function change(){
     document.getElementById("img").src = images[count]
@@ -26,4 +27,7 @@ function normal(){
 
 function show(){
     document.getElementById("hidden").style.display = "block"
+}
+function pop(){
+    window.alert("For real. Don't click this!")
 }
