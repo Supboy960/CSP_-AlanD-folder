dd19cf77aa9b24ea9e2065d91c28f47d9c6558a5
Supboy960 <alan.delara@ucas-edu.net>
2025-04-23 11:13:51 -0600

    save

--- javascript/notes.js
+++ javascript/notes.js
@@ -3,9 +3,9 @@
 let images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhcUZpl8OWqEIF8Ovv2ehl6d3IdaVWrKOm4w_VeeOK-4JN7RpHyQ&s=10&ec=72940545","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmI64E8Soa1kYDYzCrsOeQnoW2-w3MoHOT7g7otm9UqJAXKsFIbA&s=10&ec=72940545" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsisMjDMgcDz1sH_fQYqH6tq-7cT5vjgwtqjJUMSzC3kGBbPSTOg&s=10&ec=72940545" 
 ]
 function hello(){
-     let name = window.prompt("What is your name?", "Koro Sensei")
-    document.getElementById("title").innerHTML = "Hello " + name + "! You know that Challegers suck"
-   
+    let name = window.prompt("What is your name?", "Koro Sensei")
+    document.getElementById("title").innerHTML = "Hello " + name + "!"
+}
 count = 0
 function change(){
     document.getElementById("img").src = images[count]
@@ -24,10 +24,14 @@ function normal(){
     document.getElementById("btn").style.backgroundColor = "gray"
     document.getElementById("btn").style.color = "black"
 }
+function push(){
+    document.getElementById("btn").style.backgroundColor = "red"
+}
 
 function show(){
     document.getElementById("hidden").style.display = "block"
 }
+
 function pop(){
     window.alert("For real. Don't click this!")
 }
