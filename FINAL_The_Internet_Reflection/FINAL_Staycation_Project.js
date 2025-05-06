1238d70fe3119c275ae82b85b020f3ac7376506f
Supboy960 <Alan.delara@ucas-edu.net>
2025-05-06 03:50:21 +0000

    save

--- FINAL_The_Internet_Reflection/FINAL_Staycation_Project.js
+++ FINAL_The_Internet_Reflection/FINAL_Staycation_Project.js
@@ -1,14 +1,15 @@
-let images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDT2mk0bedkevmQ4OQen1jQNVdmm98j-qn6H9z_L8sPPISBx7WSw&s=10&ec=72940545","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp5hch0Xnnwblh4xQQr5wtrBZLztGZ2NEbEIESEGaPjEcl_6K2mQ&s=10&ec=72940545" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhW9mFljQ_beob2j65LtfgD2TqP9KMzh5l0LFEbk_7h_oJUX9Trw&s=10&ec=72940545" 
-]
+let images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDT2mk0bedkevmQ4OQen1jQNVdmm98j-qn6H9z_L8sPPISBx7WSw&s=10&ec=72940545",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp5hch0Xnnwblh4xQQr5wtrBZLztGZ2NEbEIESEGaPjEcl_6K2mQ&s=10&ec=72940545",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhW9mFljQ_beob2j65LtfgD2TqP9KMzh5l0LFEbk_7h_oJUX9Trw&s=10&ec=72940545"
+];
 
-count = 0
-function change(){
-    document.getElementById("img").src = images[count]
-    if(count === 2){
-        count = 0
-    }else{
-        count += 1
-    }
+let count = 0;
+
+function change() {
+    const imgElement = document.getElementById("img");
+    imgElement.src = images[count];
+    count = (count + 1) % images.length; // Loop back to the first image
 }
 
 function highlight(){
